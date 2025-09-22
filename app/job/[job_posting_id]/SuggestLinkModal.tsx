3eb4ab99f76d0ef915264d337aa67f6b28315817
AndresSalva <andres.salvatierra@ucb.edu.bo>
2025-09-22 00:09:46 -0400

    Consolidando importaciones en un mismo modulo

--- app/job/[job_posting_id]/SuggestLinkModal.tsx
+++ app/job/[job_posting_id]/SuggestLinkModal.tsx
@@ -6,8 +6,7 @@ import mixpanel from "mixpanel-browser";
 
 import { useCreateReportAdmin } from "@/lib/hooks/useCreateReportAdmin";
 import { suggestLinkSchema, type SuggestLinkFormValues } from "@/lib/schema/suggestLinkSchema";
-import { ERROR_MESSAGES } from "@/lib/errorHandling";
-import { isRateLimitError } from "@/lib/errorHandling";
+import { ERROR_MESSAGES, isRateLimitError } from "@/lib/errorHandling";
 import { CustomButton } from "@/components/CustomButton";
 
 type SuggestLinkModalProps = {
